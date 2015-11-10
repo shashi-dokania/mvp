4fb3e02323f0001eb1ce1d3d5245d1a4ab7539d5
Shashi <shashi@Shashis-MacBook-Pro.local>
2015-11-10 10:31:12 -0800

    added payment feature using external API

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,19 +2,19 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var user = require('../user/userRoutes.js');
 var list = require('../list/listRoutes.js');
-var posts = require('../posts/postRoutes.js');
 
 module.exports = function (app, express) {
 
   var userRouter = express.Router();
   var listingRouter = express.Router();
-  // var postingRouter = express.Router();
 
   app.use(morgan('dev'));
+  
   //json and url parser
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
+  
   // creating routers for each page
   app.use('/api/user', userRouter);
   app.use('/api/listing', listingRouter);
@@ -25,6 +25,7 @@ module.exports = function (app, express) {
     console.error(error.stack);
     next(error);
   });
+  
   //to send error to the client
   app.use(function (error, request, response, next) {
     response.status(500).send({ error: error.message });
@@ -32,5 +33,4 @@ module.exports = function (app, express) {
 
   user(userRouter);
   list(listingRouter);
-  // posts(postingRouter);
 };
